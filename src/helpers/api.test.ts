e188d58bd3488270a36cf14b4225817181200724
Long Zheng <long.zheng@gmail.com>
2024-11-17 15:10:31 +1100

    Switch to vitest

--- src/helpers/api.test.ts
+++ src/helpers/api.test.ts
@@ -1,4 +1,5 @@
 import { parseAuraResponseJson } from './auraApi';
+import { describe, it, expect } from 'vitest';
 
 describe('parseAuraResponseJson', () => {
     void it('parses API response', () => {
