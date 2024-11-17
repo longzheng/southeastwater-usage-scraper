a97b58cb03cf51d0e77f71179225b14eb23d2848
Long Zheng <long.zheng@gmail.com>
2024-11-17 15:10:31 +1100

    Switch to vitest

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env node */
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    modulePathIgnorePatterns: ['<rootDir>/tests/'],
-};
